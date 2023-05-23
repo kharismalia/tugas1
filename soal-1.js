acb372e9d64fbfb3e51d11e1149b213d5d4a02bd
kharisma <amalia2301kharisma@gmail.com>
2023-05-23 10:43:52 +0700

    first

---
+++ soal-1.js
@@ -0,0 +1,14 @@
+let {nama,nim,email} = {
+    nama : "Kharisma amalia",
+    nim : "01022111014",
+    email : "risma23@gmail.com"
+}
+
+console.log(nama);
+console.log(nim);
+console.log(email);
+
+const friends =["amelia","sanas"];
+const friends2 =["maypa","sasa"];
+const friends3 =["aliya","bunga"];
+console.log([...friends,...friends2,...friends3]);
